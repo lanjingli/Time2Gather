79ab8b63445567ca5898b272a0f5ec089a162e6c
Tazik Shahjahan <35576188+taziksh@users.noreply.github.com>
2023-06-26 04:37:34 -0400

    UI changes to event page (sorry should be diff branch)

--- app/src/main/java/com/example/ece_452_project/ui/DashboardScreen.kt
+++ app/src/main/java/com/example/ece_452_project/ui/DashboardScreen.kt
@@ -1,23 +1,31 @@
 package com.example.ece_452_project.ui
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -106,17 +114,24 @@ fun DashboardScreen(
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            modifier = Modifier,
-            onClick = onNewEventButtonClicked,
-            colors = ButtonDefaults.buttonColors(containerColor = SolidGreen)
-        ){
-            Text(
-                text = stringResource(R.string.new_event),
-                fontSize = 16.sp,
-                textAlign = TextAlign.Left
-            )
+        Box (contentAlignment = Alignment.Center) {
+            Button(
+                modifier = Modifier,
+                onClick = onNewEventButtonClicked,
+                colors = ButtonDefaults.buttonColors(containerColor = SolidGreen)
+            ){
+                Row () {
+                    Icon(Icons.Default.Add, contentDescription = "Add")
+                    Text(
+                        text = stringResource(R.string.new_event),
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Left
+                    )
+                }
+
+            }
         }
+
         Spacer(modifier = Modifier.height(16.dp))
         Card(
             modifier = Modifier
