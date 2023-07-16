9082619dcf1759f6d9d1ce8b7ea104d2c430958a
Leonard Li <jc9li@uwaterloo.ca>
2023-07-16 06:02:03 -0400

    intermediate development step for scrollable grids

--- app/src/main/java/com/example/ece_452_project/ui/TimeSelectionScreen.kt
+++ app/src/main/java/com/example/ece_452_project/ui/TimeSelectionScreen.kt
@@ -1,6 +1,8 @@
 package com.example.ece_452_project.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,8 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
@@ -24,6 +36,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,7 +61,18 @@ fun TimeSelectionScreen(
                 text = title,
                 style = MaterialTheme.typography.headlineMedium
         )
-
+        val numRows = 12
+        //val lazyGridState = rememberLazyGridState()
+        val itemsList = (1..100).toList()
+        val itemModifier = Modifier.border(1.dp, Color.Blue).width(80.dp).wrapContentSize()
+        LazyHorizontalGrid(
+                rows = GridCells.Fixed(numRows),
+                modifier = Modifier.fillMaxSize()
+        ) {
+            items(itemsList) {
+                Text("Item is $it", itemModifier)
+            }
+        }
         Button(
                 modifier = Modifier
                         .fillMaxWidth(),
@@ -63,14 +87,13 @@ fun TimeSelectionScreen(
 
 @Preview(showBackground = true)
 @Composable
-fun TimeSelectionScreen(){
-    ListSelectScreen(
+fun TimeSelectionPreview(){
+    TimeSelectionScreen(
             modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp),
-            title = "Options",
-            options = listOf<String>("Option 1", "Option 2", "Option 3"),
-            onNextButtonClicked = {selected: List<Boolean> -> Unit}
+            title = "Test",
+            onNextButtonClicked = {selected: List<LocalDateTime> -> Unit}
     )
 }
 
