8a23be15be9d5b74dcd8a808a0fec1e47e238321
enjineerMan <david.wang236@gmail.com>
2023-07-26 08:45:27 -0400

    added fragment container to make location fragment compatible with compose. Still need to figure out dependencies and test

--- app/src/main/java/com/example/ece_452_project/ui/MapScreen.kt
+++ app/src/main/java/com/example/ece_452_project/ui/MapScreen.kt
@@ -3,6 +3,14 @@ package com.example.ece_452_project.ui
 import com.example.ece_452_project.data.DummyPlace
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.supportFragmentManager
+import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.mutableStateOf
+import android.view.View
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.remember
 /**
@@ -15,7 +23,48 @@ fun MapScreen(
 ) {
 
     // not sure if this is right. I also tried just 'locationFragment().LocationScreen()'
-    val locationFragment = remember { LocationFragment() }
-    locationFragment.LocationScreen()
+//    val locationFragment = remember { LocationFragment() }
+//    locationFragment.LocationScreen()
+
+
+        FragmentContainer(
+            modifier = Modifier.fillMaxSize(),
+            fragmentManager = getFragmentManager,
+            commit = { add(it, locationScreen()) }
+        )
 }
 
+@Composable
+fun FragmentContainer(
+    modifier: Modifier = Modifier,
+    fragmentManager: FragmentManager,
+    commit: FragmentTransaction.(containerId: Int) -> Unit
+) {
+    val containerId by rememberSaveable { mutableStateOf(View.generateViewId()) }
+    var initialized by rememberSaveable { mutableStateOf(false) }
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            FragmentContainerView(context)
+                .apply { id = containerId }
+        },
+        update = { view ->
+            if (!initialized) {
+                fragmentManager.commit { commit(view.id) }
+                initialized = true
+            } else {
+                fragmentManager.onContainerAvailable(view)
+            }
+        }
+    )
+}
+
+/** Access to package-private method in FragmentManager through reflection */
+private fun FragmentManager.onContainerAvailable(view: FragmentContainerView) {
+    val method = FragmentManager::class.java.getDeclaredMethod(
+        "onContainerAvailable",
+        FragmentContainerView::class.java
+    )
+    method.isAccessible = true
+    method.invoke(this, view)
+}
